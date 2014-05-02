014aec4845807f103ac9944f783680eeacad3b10
eaduart <eaduart@eaduarts-MacBook-Air.local>
2014-05-01 22:49:56 -0700

    Initial Commit

---
+++ Kaitlyn/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Kaitlyn
+//
+//  Created by eaduart on 5/1/14.
+//  Copyright (c) 2014 eaduart. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
